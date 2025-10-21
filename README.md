ccd5e3b380d65deebd703b82f49d3c1309a46288
Jiwan Chung <jiwan.chung.research@gmail.com>
2025-10-21 16:37:33 +0900

    batch run

---
+++ README.md
@@ -0,0 +1,21 @@
+stitch2D-CLI
+========
+
+Python Library to stitch microscopic images.
+
+
+Install
+-------
+
+Install with pip:
+
+```bash
+pip install https://github.com/JiwanChung/stitch2d_cli.git
+```
+
+Quick start
+-----------
+
+```bash
+stitch2d-cli ./examples/inputs2/0.png ./examples/inputs2/1.png -o ./examples/output2.png
+```
--- src/stitch2d_cli/cli.py
+++ src/stitch2d_cli/cli.py
@@ -1,6 +1,6 @@
-import typer
 from pathlib import Path
 
+import typer
 import numpy as np
 import cv2
 from skimage.io import imsave
@@ -46,8 +46,6 @@ def pair(
     """Perform ImageJ-like 2D stitching with bandpass and NCC refinement."""
     I1, I2 = read_gray_f32(file1), read_gray_f32(file2)
     I2, _, _ = match_global_mean_var(I1, I2)
-    print(I1.mean())
-    print(I2.mean())
 
     dx, dy, resp = fft_phase_correlation(I1, I2)
     typer.echo(f"[PhaseCorr] dx={dx:.2f}, dy={dy:.2f}, resp={resp:.3f}")
@@ -57,6 +55,51 @@ def pair(
     output.parent.mkdir(parents=True, exist_ok=True)
     imsave(str(output), (np.clip(fused, 0, 1) * 255).astype(np.uint8))
     typer.echo(f"✅ Saved fused image → {output}")
+
+
+def do_stitch(I1, I2):
+    I2, _, _ = match_global_mean_var(I1, I2)
+
+    dx, dy, resp = fft_phase_correlation(I1, I2)
+    typer.echo(f"[PhaseCorr] dx={dx:.2f}, dy={dy:.2f}, resp={resp:.3f}")
+
+    fused = fuse_two_images(I1, I2, (dx, dy))
+    return fused
+
+
+def iter_files(paths: list[Path]) -> np.ndarray:
+    paths = list(sorted(paths, key=lambda x: x.stem))
+    head, rest = paths[0], paths[1:]
+    curr_image = read_gray_f32(head)
+    for next_path in rest:
+        next_image = read_gray_f32(next_path)
+        fused = do_stitch(curr_image, next_image)
+        next_image = fused
+    return fused
+
+
+@app.command()
+def batch(path: str):
+    out_dir = Path(path) / "outputs"
+    out_dir.mkdir(exist_ok=True, parents=True)
+    n = 0
+    for dir in Path(path).glob("*"):
+        if not dir.is_dir():
+            continue
+        if dir.name == "outputs":
+            continue
+
+        inputs = [*dir.glob("*.png"), *dir.glob("*.jpg")]
+        if len(inputs) < 2:
+            continue
+        fused = iter_files(inputs)
+        out_path = out_dir / (dir.name + ".png")
+
+        imsave(str(out_path), (np.clip(fused, 0, 1) * 255).astype(np.uint8))
+        n += 1
+        typer.echo(f"saved fused image -> {out_path}")
+
+    typer.echo(f"completed directory {path}: {n} images saved")
 
 
 def main():
